b178d90717ede63d5f569271455bb26d1d8f919c
AspBenjamin <benjaminviktiga@gmail.com>
2025-04-14 15:35:41 +0200

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+operator = input("What type of math question? (+ - * /): ")
+num1 = float(input("What's the first number? "))
+num2 = float(input("What's the second number? "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not valid, I can only calculate addition, subtraction, multiplication and fractions.")
